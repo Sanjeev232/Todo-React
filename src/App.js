37e4cd6f110f49b60409e39d94423445cdcc4246
Sanjeev <contactsanjeev.singh@yahoo.com>
2019-12-30 01:21:19 +0530

    changed class

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-
- 
-
   render() {
     return (
       <div className="container">
         <header className="header">
           <h1 className="main-heading">Todo List</h1>
-          <button className="add-button">+</button>
+          <button className="add-buttoon">+</button>
         </header>
       </div>
     );
