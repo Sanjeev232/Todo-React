569ea561e4eb1c633744f072d60d553564569e6a
Sanjeev <contactsanjeev.singh@yahoo.com>
2020-01-02 17:12:20 +0530

    Updates

---
+++ precache-manifest.da25fb856fc10266101241c0ac30f186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14458f610d2d1726cb4b5e6bee83ef6",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "7bdab3c028908091130b",
+    "url": "/Todo-React/static/css/main.0df9e852.chunk.css"
+  },
+  {
+    "revision": "83d80f0620c3e6202708",
+    "url": "/Todo-React/static/js/2.b97a247f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo-React/static/js/2.b97a247f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bdab3c028908091130b",
+    "url": "/Todo-React/static/js/main.ceecee45.chunk.js"
+  },
+  {
+    "revision": "83c0a94fce1f58ccb332",
+    "url": "/Todo-React/static/js/runtime-main.72d9925a.js"
+  }
+]);
